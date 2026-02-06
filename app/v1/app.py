bd9c15435b1213f1eecdf72b289753969bc2f992
Thiru-2004-varul/ <thiruvarulg9@gmail.com>
2026-02-06 09:28:42 -0500

    implementation of python  with flask

---
+++ app/v1/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "v1"
+
+@app.route("/")
+def home():
+    return f"Hello from Virtual Server {VERSION}\n"
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
